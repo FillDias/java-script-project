7295b2c9dc1713c271835025b9651fa8956f2748
Felipe <dias.felipecarlos@gmail.com>
2025-05-11 12:30:31 -0300

    Script de informação de itens na lista

--- index.js
+++ index.js
@@ -42,6 +42,20 @@ itemDaLista.appendChild(itemData)
 listaDeCompras.appendChild(itemDaLista);
 
 
-
+verificarListaVazia();
 })
 
+
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if(itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block"
+    } else{
+        mensagemListaVazia.style.display = "none"
+    }
+}
+
+verificarListaVazia();
